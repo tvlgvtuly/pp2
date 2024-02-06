cea71841a9480924a6cbc8b0090018ce098b070d
Tastemir <tastemir.tt@gmail.com>
2024-02-07 02:21:39 +0600

    Function

---
+++ lab 3/Functions.py
@@ -0,0 +1,24 @@
+#1
+def my_function():
+    print("hello from the function")
+
+#2
+def my_function():
+    print("hello from the function")
+my_function()
+
+#3
+def my_function(fname, lname):
+  print(fname)
+
+#4
+def my_function(x):
+   return x + 5
+
+#5
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+#6
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
