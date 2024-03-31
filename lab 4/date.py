7bb71a15c25db751187a2eed7e410313bcd26c6a
Tastemir <tastemir.tt@gmail.com>
2024-03-31 16:40:57 +0600

    arkanoid

--- lab 4/date.py
+++ lab 4/date.py
@@ -1,3 +1,12 @@
+"""
+Напишите программу на Python, которая вычитает пять дней из текущей даты.
+
+Напишите программу на Python для печати вчера, сегодня, завтра.
+
+Напишите программу на Python, чтобы отбрасывать микросекунды из даты и времени.
+
+Напишите программу на Python для расчета разницы в двух датах в секундах.
+"""
 from datetime import date, timedelta, datetime
 #1
 today = date.today()
@@ -11,7 +20,7 @@ print(f'yesterday: {today - timedelta(days = 1)}\ntoday: {today}\ntomorrow: {tod
 #3
 dt = datetime.now()
 dt = dt.replace(microsecond=0)
-print(dt, '.000000', sep = '')
+print(dt, '.00', sep = '')
 #4
 
 def difference_in_seconds(date1, date2):
